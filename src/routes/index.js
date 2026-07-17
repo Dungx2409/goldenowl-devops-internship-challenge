8149eca648c712c92b8892bbcb255ef53c78e017
DungLuong <dungpro2409@gmail.com>
2026-07-17 17:05:23 +0700

    Test workflow deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', (req, res) => {
     const responseJson = {
-        message: 'Welcome warriors to Golden Owl!',
+        message: 'Welcome warriors to Golden Owl!. My name is Dung Luong and I am a DevOps Intern here. I am excited to be part of this amazing team and looking forward to contributing to the success of the company. Thank you for having me on board!',
     }
     res.json(responseJson)
 })
