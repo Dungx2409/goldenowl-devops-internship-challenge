18cc6adee10e6c8be8c9aa2a1b54d51c7ca20bf1
DungLuong <dungpro2409@gmail.com>
2026-07-17 17:06:57 +0700

    update test case

--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -8,7 +8,7 @@ describe('Test Endpoints', () => {
         expect(response.status).toBe(200)
         expect(response.body).toHaveProperty(
             'message',
-            'Welcome warriors to Golden Owl!'
+            'Welcome warriors to Golden Owl!. My name is Dung Luong and I am a DevOps Intern here. I am excited to be part of this amazing team and looking forward to contributing to the success of the company. Thank you for having me on board!'
         )
         expect(response.header['content-type']).toMatch(/json/)
     })
