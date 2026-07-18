0228cd559624d6d4211412a3c0aec7fc4c63c1e3
DungLuong <dungpro2409@gmail.com>
2026-07-18 16:34:15 +0700

    Test: Flows deloy app

--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -8,7 +8,7 @@ describe('Test Endpoints', () => {
         expect(response.status).toBe(200)
         expect(response.body).toHaveProperty(
             'message',
-            'Welcome warriors to Golden Owl!.'
+            'Welcome warriors to Golden Owl!. My name is Dung!'
         )
         expect(response.header['content-type']).toMatch(/json/)
     })
